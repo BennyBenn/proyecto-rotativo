f4c4c0ab6a44b3812bdb448b14c8c73b2e83a0f7
kevinkalvin62 <134903820+kevinkalvin62@users.noreply.github.com>
2023-07-02 21:29:27 -0600

    Add files via upload
    creacion de clases ususario, administrador y superUsusario

---
+++ mavenproject1/administrador.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author kevin
+ */
+public class administrador extends usuario{
+      String datos;
+  public void IngresarDatos(String datos){
+      
+  }
+  public String GetDatos(){
+      return datos;
+  }
+  // permitirAcceso y denegarAcceso se encargaran de permitir acciones a los super usuarios
+  public void denegarAccseso(){
+  
+  }
+  public void permitirAcceso(){
+  
+  }
+}
